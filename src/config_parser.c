fd4c4e07ad51a42056e0f4aa44570897028d4d06
Adam Majer <amajer@suse.de>
2021-04-09 14:12:03 +0200

    Add (L)GPL 3.0 as license

--- src/config_parser.c
+++ src/config_parser.c
@@ -1,3 +1,20 @@
+/*  libalternatives - update-alternatives alternative
+ *  Copyright (C) 2021  SUSE LLC
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
@@ -125,7 +142,7 @@ struct ConfigParserState* initConfigParser(const char *binary_name)
   if (binary_name == NULL)
     return NULL;
 
-  struct ConfigParserState *state = malloc(sizeof(struct ConfigParserState));
+  struct ConfigParserState *state = (struct ConfigParserState*)malloc(sizeof(struct ConfigParserState));
 
   state->binary_name = strdup(binary_name);
   state->priority = 0;
--- src/default_helper.c
+++
@@ -1,6 +0,0 @@
-#include "libalternatives.h"
-
-int main(__attribute__((unused)) int argc, char *argv[])
-{
-	return execDefault(argv);
-}
