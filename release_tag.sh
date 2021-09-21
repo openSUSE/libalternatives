5118eb13fbe0f40f62db098612e840383a47aa1d
Neal Gompa <ngompa13@gmail.com>
2021-09-21 08:24:59 -0400

    cmake: Build and install CMake and PkgConfig files
    In order to make it easy for other projects to depend on libalternatives,
    we want them to be able to detect it in their build scripts to properly
    depend on the library.
    
    This introduces a CMake module script and a pkgconfig file to support
    virtually all build tools that support dependencies by either of those
    systems.
    
    As part of this, we bump the minimum version to CMake 3.12 because
    that version introduces most of the built-in modules for generating
    CMake config modules.

--- release_tag.sh
+++ release_tag.sh
@@ -14,11 +14,13 @@ fi
 if [ "$1" = "-s" ]; then
 	git status | grep -q 'nothing to commit, working tree clean' || exec echo Pending commits
 
+	ver=$(grep LIBALTS_VERSION config.h | awk '{print $3}' | sed 's,",,g')
 	echo "Tagging new release v$2"
 	echo $2 | grep '^[0-9]\+\.[0-9]\+$' || exec echo 'Invalid version format'
 
 	sed -e "s,@PROJECT_VERSION@,$2," -e "s,@PROJECT_YEAR@,`date +%Y`," config.h.in > config.h
-	git add config.h &&
+	sed -e "s,VERSION ${ver}, VERSION $2," -i CMakeLists.txt
+	git add config.h CMakeLists.txt &&
 	git commit -m "Releasing version $2" &&
 	git tag v$2
 	exit 0
